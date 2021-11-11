7be837998989360fd3af63e886b868524c9e6aa1
Paweł Ksieniewicz <pawel.ksieniewicz@pwr.edu.pl>
2021-11-11 14:55:36 +0100

    Prequential.

--- docs/source/evaluators_api.rst
+++ docs/source/evaluators_api.rst
@@ -1,3 +1,12 @@
 #################
 Evaluators module
 #################
+
+.. currentmodule:: strlearn.evaluators
+
+.. autosummary::
+  Prequential
+
+.. automodule:: strlearn.evaluators
+  :members:
+  :show-inheritance:
--- strlearn/evaluators/TestThenTrain.py
+++ strlearn/evaluators/TestThenTrain.py
@@ -1,5 +1,3 @@
-"""Test-Than-Train data stream evaluator."""
-
 import numpy as np
 from sklearn.base import ClassifierMixin
 from sklearn.metrics import accuracy_score
@@ -9,40 +7,6 @@ from ..metrics import balanced_accuracy_score
 
 
 class TestThenTrain:
-    """
-    Test Than Train data stream evaluator.
-
-    Implementation of test-then-train evaluation procedure,
-    where each individual data chunk is first used to test
-    the classifier and then it is used for training.
-
-    Attributes
-    ----------
-    classes_ : array-like, shape (n_classes, )
-        The class labels.
-    scores_ : array-like, shape (stream.n_chunks, 5)
-        Values of accuracy_score, roc_auc_score,
-        geometric_mean_score, balanced_accuracy_score and f_score for
-        each processed data chunk.
-
-    Examples
-    --------
-    >>> import strlearn as sl
-    >>> stream = sl.streams.StreamGenerator()
-    >>> clf = sl.classifiers.AccumulatedSamplesClassifier()
-    >>> evaluator = sl.evaluators.TestThenTrainEvaluator()
-    >>> evaluator.process(clf, stream)
-    >>> print(evaluator.scores_)
-    ...
-    [[0.92       0.91879699 0.91848191 0.91879699 0.92523364]
-    [0.945      0.94648779 0.94624912 0.94648779 0.94240838]
-    [0.92       0.91936979 0.91936231 0.91936979 0.9047619 ]
-    ...
-    [0.92       0.91907051 0.91877671 0.91907051 0.9245283 ]
-    [0.885      0.8854889  0.88546135 0.8854889  0.87830688]
-    [0.935      0.93569212 0.93540766 0.93569212 0.93467337]]
-    """
-
     def __init__(
         self, metrics=(accuracy_score, balanced_accuracy_score), verbose=False
     ):
@@ -53,16 +17,6 @@ class TestThenTrain:
         self.verbose = verbose
 
     def process(self, stream, clfs):
-        """
-        Perform learning procedure on data stream.
-
-        Parameters
-        ----------
-        clf : scikit-learn estimator
-            Classifier implementing a `partial_fit()` method.
-        stream : object
-            Data stream as an object.
-        """
         # Verify if pool of classifiers or one
         if isinstance(clfs, ClassifierMixin):
             self.clfs_ = [clfs]
